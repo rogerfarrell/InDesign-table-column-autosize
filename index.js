1dc2c4ffdbfb901981ad996a16d0b5a99dde5e12
Roger Farrell <127912262+RogerFarrell@users.noreply.github.com>
2025-05-19 17:41:08 -0500

    sexy rewrite

--- index.js
+++ index.js
@@ -21,13 +21,42 @@ const autofitColumns =
   {
     try
     {
-      if ( invalidSelection(app) )
-        throw new Error("Please select one or more table cells.");
+      if ( invalidSelection(app) ) throw new Error("Please select one or more table cells.");
 
-      const selectedCells = app.selection[0].cells;
-      const cells = selectedCells.everyItem().getElements();
+      const minColumnWidth = 3;    // This is the minimum allowed by InDesign.
+      const maxColumnWidth = 500;
 
-      autosizeCells(cells);
+      const selectedColumns       = getSelectedColumns(app.selection[0]);
+      const selectedCells         = app.selection[0].cells.everyItem().getElements();
+      const selectedNormalCells   = selectedCells.filter( cell => cell.columnSpan == 1 );
+      const selectedSpanningCells = selectedCells.filter( cell => cell.columnSpan > 1 );
+
+      selectedColumns.forEach(
+        column =>
+        {
+          const cells =
+            [
+              ...selectedNormalCells
+                .filter( cell => cell.parentColumn.index == column.index ),
+              ...selectedSpanningCells
+                .filter( cell => ((cell.parentColumn.index + cell.columnSpan) - 1) == column.index )
+            ];
+
+          const bestWidth =
+            binarySearch( minColumnWidth, maxColumnWidth,
+              trialWidth =>
+              {
+                column.width = trialWidth;
+                app.activeDocument.recompose();
+
+                if ( cellsOverflow(cells) ) return false;
+                                            return true;
+              }
+            );
+
+          column.width = bestWidth;
+        }
+      );
     }
     catch (e)
     {
@@ -63,6 +92,37 @@ const showAlert =
     return;
   };
 
+const getSelectedColumns =
+  selection =>
+  {
+    const selectedColumns = selection.columns.everyItem().getElements();
+
+    const table = getParentTable( selection.columns.firstItem() );
+    const lastSelectedColumn = selection.columns.lastItem();
+    const lastColumnCells = lastSelectedColumn.cells.everyItem().getElements();
+    const maxLastColumnSpan = lastColumnCells.reduce( (max, cell) => Math.max(max, cell.columnSpan), 1);
+    const lastSpannedColumnIndex = (lastSelectedColumn.index + maxLastColumnSpan) - 1;
+    const extraSpannedColumns =
+      table.columns.everyItem().getElements()
+        .filter(
+          column => column.index > lastSelectedColumn.index
+                    && column.index <= lastSpannedColumnIndex
+        );
+
+    return [...selectedColumns, ...extraSpannedColumns];
+  };
+
+const getParentTable =
+  object =>
+  {
+    if ( object.constructor.name === "Table" ) return object;
+                                               return getParentTable(object.parent);
+  };
+
+const cellsOverflow =
+  cells =>
+    cells.some( cell => cell.overflows == true );
+
 const binarySearch =
   (min, max, test) =>
   {
@@ -80,117 +140,4 @@ const binarySearch =
       };
 
     return search(min, max, max); // initial best is max fallback
-  };
-
-const isTrulyEmpty =
-  cell =>
-  {
-      return cell.contents == "" && !cell.overflows;
-      // cell.contents returns "" if all content is overflowed.
-  };
-
-const getParentTable =
-  object =>
-  {
-    if ( object.constructor.name === "Table" ) return object;
-                                               return getParentTable(object.parent);
-  };
-
-const resize = 
-  (elementToResize, elementToTest, min, max) =>
-  {
-     const bestWidth =
-       binarySearch( min, max,
-         trialWidth =>
-         {
-           elementToResize.width = trialWidth;
-           app.activeDocument.recompose();
-
-           if ( elementToTest.overflows ) return false;
-                                          return true;
-         }
-       );
-
-       elementToResize.width = bestWidth;
-  };
-
-const autosizeCells =
-  cells =>
-  {
-    const maxWidth = 500;
-    const minWidth = 3;
-    // 3 is the minimum allowed for col width.
-    // Setting the minimum lower will result in an error.
-    // (InDesign interprets these units as points.)
-
-    const nonEmptyCells = cells.filter( cell => !isTrulyEmpty(cell) );
-    nonEmptyCells.forEach( cell => cell.parentColumn.autoGrow = false );
-    // autoGrow must be false for cell contents to overflow predictably.
-
-    const columnIndices =
-      [...new Set( nonEmptyCells.map( cell => cell.parentColumn.index ) )];
-    const columns =
-      columnIndices.map( index => ({ index: index, minWidth: minWidth }) );
-
-    const normalCells   = nonEmptyCells.filter( cell => cell.columnSpan == 1 );
-    const spanningCells = nonEmptyCells.filter( cell => cell.columnSpan >  1 );
-    // Spanning (merged) cells are handled separately at the end.
-    // This side-steps complicated column width calculations. 
-
-    normalCells.forEach(
-      normalCell =>
-      {
-        const currentColumn =
-          columns.find( column => column.index == normalCell.parentColumn.index );
-        const minWidth = currentColumn.minWidth;
-
-        resize(normalCell, normalCell, minWidth, maxWidth);
-
-        if ( normalCell.width >= currentColumn.minWidth )
-        {
-          currentColumn.minWidth = normalCell.width;
-        }
-      }
-    );
-
-    spanningCells.forEach(
-      spanningCell =>
-      {
-        const firstParentColumnIndex = spanningCell.parentColumn.index;
-        const lastParentColumnIndex = (firstParentColumnIndex + spanningCell.columnSpan) - 1;
-        const parentColumnIndices =
-          Array.from(
-            { length: lastParentColumnIndex - firstParentColumnIndex + 1 },
-            (_, i) => firstParentColumnIndex + i
-          );
-        const parentTable = getParentTable(spanningCell);
-        const parentColumnAt = index => parentTable.columns.item(index);
-
-        parentColumnIndices .forEach(
-          parentColumnIndex =>
-          {
-            parentColumnAt(parentColumnIndex).width = maxWidth;
-          }
-            // Sets all the parent columns to maxWidth to start the calc.
-        );
-
-        parentColumnIndices.forEach(
-          parentColumnIndex =>
-          {
-            const parentColumnIsEmpty =
-              !columns.some( (column) => parentColumnIndex == column.index );
-
-            const minWidth =
-              parentColumnIsEmpty
-                ? 3
-                : columns
-                    .find( column => column.index == parentColumnIndex )
-                    .minWidth;
-            //TODO I would like to replace this with something more elegant.
-
-            resize(parentColumnAt(parentColumnIndex), spanningCell, minWidth, maxWidth);
-          }
-        );
-      }
-    );
   };
